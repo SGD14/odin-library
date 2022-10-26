81e705a702833ee89fc4819d741be9b012d4d4eb
Dani <24252795+SGD14@users.noreply.github.com>
2022-10-26 13:30:29 +0200

    Refactor table building logic into a function

--- javascript.js
+++ javascript.js
@@ -11,35 +11,41 @@ function Book(title, author, pages, read) {
     }
 };
 
+function rebuildBookTable() {
+    let bookTable = document.querySelector("#book-table");
+
+    bookTable.childNodes.forEach(child => bookTable.removeChild(child));
+
+    if(booksLibrary.length > 0) {
+        booksLibrary.forEach(book => {
+            let tableRow = document.createElement("tr");
+
+            let titleColumn = document.createElement("td");
+            let authorColumn = document.createElement("td");
+            let pagesColumn = document.createElement("td");
+            let readColumn = document.createElement("td");
+
+            titleColumn.textContent = book.title;
+            authorColumn.textContent = book.author;
+            pagesColumn.textContent = book.pages;
+            readColumn.textContent = book.read ? "Yes" : "No";
+
+            tableRow.appendChild(titleColumn);
+            tableRow.appendChild(authorColumn);
+            tableRow.appendChild(pagesColumn);
+            tableRow.appendChild(readColumn);
+
+            bookTable.appendChild(tableRow);
+        });
+    }
+}
+
 let book1 = new Book("Dummy Book 1", "Dummy Author", 203, true);
 let book2 = new Book("Dummy Book 2", "Dummy Author", 386, false)
 
 booksLibrary.push(book1, book2);
 
-let bookTable = document.querySelector("#book-table");
-
-if(booksLibrary.length > 0) {
-    booksLibrary.forEach(book => {
-        let tableRow = document.createElement("tr");
-
-        let titleColumn = document.createElement("td");
-        let authorColumn = document.createElement("td");
-        let pagesColumn = document.createElement("td");
-        let readColumn = document.createElement("td");
-
-        titleColumn.textContent = book.title;
-        authorColumn.textContent = book.author;
-        pagesColumn.textContent = book.pages;
-        readColumn.textContent = book.read ? "Yes" : "No";
-
-        tableRow.appendChild(titleColumn);
-        tableRow.appendChild(authorColumn);
-        tableRow.appendChild(pagesColumn);
-        tableRow.appendChild(readColumn);
-
-        bookTable.appendChild(tableRow);
-    });
-}
+rebuildBookTable();
 
 document.querySelector("#add-new-book-button").addEventListener('click', event => {
     document.querySelector('#new-book-form').style.display = "flex";
