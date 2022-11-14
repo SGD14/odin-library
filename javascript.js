15d720b5efb1013b606aaaa0193edbb10645bcc4
Dani <24252795+SGD14@users.noreply.github.com>
2022-11-14 16:54:43 +0100

    Change Book constructor function to class syntax

--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
 let booksLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-    this.info = function() {
+    info() {
         return title + " by " + author + ", " + pages + " pages, " + read ? "already read" : "not read yet";
     }
-};
+}
 
 function rebuildBookTable() {
     let bookTable = document.querySelector("#book-table");
